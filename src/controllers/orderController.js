addfefe0cb2d1dd8cfc6bd15b0825a9f0925a570
Eliu2001 <villalobosgermain2@gmail.com>
2025-11-05 17:32:58 -0300

    Add orderController with createOrder and getOrdersFromUser functions

---
+++ src/controllers/orderController.js
@@ -0,0 +1,26 @@
+import { Order } from "../models/order";
+import { User } from "../models/user";
+
+export const createOrder = async (req, res) => {
+    try {
+        const { userId } = req.params;
+        const user = await User.findByPk(userId);
+
+        if (!user) return res.status(404).json({ message : "User not found"});
+        
+        const order = await Order.create(req.body);
+        res.status(201).json(order);
+    } catch (error) {
+        res.status(500).json({ message: "Internal server error" });
+    }
+};
+
+export const getOrdersFromUser = async (req, res) => {
+    try {
+        const { userId } = req.params;
+        const order = await Order.findAll({ where : { userId}});
+        res.status(200).json(order);
+    } catch (error) {
+        res.status(500).json({ message: "Internal server error" });
+    }
+}
